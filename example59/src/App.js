56a8093347108d4f7a2fa6e1b06592bbca7125fc
Vishwanath Rao <vishymails@gmail.com>
2022-07-15 13:41:20 +0530

    Add files via upload

---
+++ example59/src/App.js
@@ -0,0 +1,13 @@
+import { FetchData } from "./demo";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <FetchData />
+    </div>
+   );
+}
+
+export default App;
---
+++ example60/src/App.js
@@ -0,0 +1,13 @@
+import { FetchData } from "./demo";
+import {Fragment } from 'react'
+import Header from './components/Layout/Header';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+   );
+}
+
+export default App;
