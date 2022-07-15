d638a3b09fb557327bbbdc37163b01125e3c76f1
Vishwanath Rao <vishymails@gmail.com>
2022-07-15 13:39:38 +0530

    Add files via upload

---
+++ example46/src/App.js
@@ -0,0 +1,17 @@
+import { Route } from 'react-router-dom';
+
+import AddUser from './components/Users/AddUser';
+
+
+
+
+function App() {
+  return (
+    <div>
+      <AddUser />
+      
+    </div>
+  );
+}
+
+export default App;
